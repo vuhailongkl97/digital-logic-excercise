90e3e39c0ce1ba356280d3ddf8ae790cfbb00ed3
vuhailongkl97 <vuhailongkl97@gmail.com>
2019-05-07 07:45:11 +0700

    fix display data

--- do_an_so/main.vhd
+++ do_an_so/main.vhd
@@ -4,20 +4,19 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity stepper_test is
+entity counter_person is
     Port ( 
 			clk : in  STD_LOGIC;
-			sw_dir : in  STD_LOGIC;
 			sw_enable : in  STD_LOGIC;
-			sw_speed_up : in STD_LOGIC;				-- hong ngoai 1 as button1
-			sw_speed_down: in STD_LOGIC;			-- hong ngoai 2 as button2
+			sensor_1 : in STD_LOGIC;				-- hong ngoai 1 as button1
+			sensor_2: in STD_LOGIC;			-- hong ngoai 2 as button2
 			lcd_rw	: out std_logic;   				-- read & write control
 			lcd_e 	: out std_logic;   				-- enable control
 			lcd_rs	: out std_logic;   				-- data or command control
 			data  	: out std_logic_vector(7 downto 0)); -- data field for transfer from fpga to lcd
-end stepper_test;
+end counter_person;
 
-architecture Behavioral of stepper_test is
+architecture Behavioral of counter_person is
 	signal count : unsigned(18 downto 0):= (others => '0');
 	signal step_count: unsigned(7 downto 0):= (others => '0');
 	signal s_speed: integer := 5;
@@ -34,21 +33,11 @@ architecture Behavioral of stepper_test is
 	--------------------------------------------------
 	constant N: integer :=19; 
 	type arr is array (1 to N) of std_logic_vector(7 downto 0);
-	--signal datas : arr :=    (X"38",X"0c",X"06",X"01",X"80",
-	--X"20",x"20",x"20",x"72",x"70",x"49");
-	
-	--X"38",X"0c",X"06",X"01",X"80", bytes for init lcd 
-	--X"20",x"20",x"20",             bytes for space 
-	--x"72",x"70",x"6D"				 bytes for display string "rpm"
-	
 	-- update string for team
 	signal datas : arr :=    (X"38",X"0c",X"06",X"01",X"80",
 	x"10",x"20",x"20",	-- > number_persons_in 78
 	x"20",x"11",x"20",x"20",	-- < number_persion_out  11 12
 	x"20",x"50",x"45",x"52",x"53",x"4F",x"4E");
-	-- endline with 0xC0
-	-- > 0x10
-	-- < 0x11
 	type dec is array (0 to 9) of std_logic_vector(7 downto 0);
 	signal number : dec := (X"30", X"31",X"32", X"33",X"34", X"35",X"36", X"37",X"38", X"39");
 	--------------------------------------------------
@@ -67,18 +56,19 @@ Div : process(clk)
 		end if; 
 	end process; 
 -----------------------------------------------
-tocdo: process
+countp: process
 variable check : integer := 0;
 begin
 	if(falling_edge(clk_1hz)) then 
-		if(sw_speed_up = '1') then 
+		if(sensor_1 = '1') then 
 			if ( check = 3 ) then 
 					check := 0;
 					-- tang so nguoi ra ngoai 
-					nguoi_ra(0) <= nguoi_ra(0) + 1;
-					if (nguoi_ra(0) = 10) then 
+					if (nguoi_ra(0) = 9) then 
 						nguoi_ra(1) <= nguoi_ra(1) + 1;
 						nguoi_ra(0) <= 0;
+					else 
+						nguoi_ra(0) <= nguoi_ra(0) + 1;
 					end if;
 			else 
 				if ( check = 1) then 
@@ -87,14 +77,15 @@ begin
 					check := 1;
 				end if;
 			end if;
-		elsif(sw_speed_down = '1') then 
+		elsif(sensor_2 = '1') then 
 			 if (check = 1) then
 					check := 0; -- reset state 
 					--tang so nguoi vao trong 
-					nguoi_vao(0) <= nguoi_vao(0) + 1;
-					if (nguoi_vao(0) = 10) then 
+					if (nguoi_vao(0) = 9) then 
 						nguoi_vao(1) <= nguoi_vao(1) + 1;
 						nguoi_vao(0) <= 0;
+					else 
+						nguoi_vao(0) <= nguoi_vao(0) + 1;
 					end if;
 			else 
 				if ( check = 3 ) then 
@@ -132,7 +123,7 @@ lcd : process(clk)
 				else data <= X"0";
 				end if; 
 			elsif(j = 8) then
-				if( nguoi_vao(1) = 0) then data <= X"31"; 
+				if( nguoi_vao(0) = 0) then data <= X"31"; 
 					elsif( nguoi_vao(0) = 2) then data <= X"32"; 
 					elsif( nguoi_vao(0) = 3) then data <= X"33"; 
 					elsif( nguoi_vao(0) = 4) then data <= X"34"; 
